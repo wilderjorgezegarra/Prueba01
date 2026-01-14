df5889607c5846addc0e2eff7d4c5bba8b3adc2f
WilderJorge <wilder@gmail.com>
2026-01-14 18:16:32 -0500

    modificando01 comentario01

---
+++ src/main/java/com/hackathon/demo/FlightontimeApplication.java
@@ -0,0 +1,19 @@
+package com.hackathon.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightontimeApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightontimeApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hola Equipo, H12-25-L-Equipo 18-Data Science");
+		System.out.println("Proyecto MVP para predecir retrasos en vuelos usando Data Science y API en Java Spring Boot");
+	}
+}
