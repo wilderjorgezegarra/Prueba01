f510494b3ea392636da243516e5fbe574eca8c87
WilderJorge <wilder@gmail.com>
2026-01-14 19:01:14 -0500

    Actualización02 de la aplicación02

--- src/main/java/com/hackathon/demo/FlightontimeApplication.java
+++ src/main/java/com/hackathon/demo/FlightontimeApplication.java
@@ -15,5 +15,9 @@ public class FlightontimeApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		System.out.println("Hola Equipo, H12-25-L-Equipo 18-Data Science");
 		System.out.println("Proyecto MVP para predecir retrasos en vuelos usando Data Science y API en Java Spring Boot");
+
+		System.out.println("Hola Wilder,  como te va...");
+		System.out.println("Me va Super Bien!!!");
 	}
 }
+
